2a5d388f067314298507546e3fff3beeebe6d622
AyAztuB <ayaztub@gmail.com>
2025-02-08 14:14:04 +0100

    adding new include

--- include/ayaztub/utils.h
+++ include/ayaztub/utils.h
@@ -2,6 +2,7 @@
 #define __AYAZTUB__UTILS_H__
 
 #include <ayaztub/utils/util_attributes.h>
+#include <ayaztub/utils/util_macros.h>
 #include <ayaztub/utils/assert.h>
 #include <ayaztub/utils/debug.h>
 
